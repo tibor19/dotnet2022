94430cbb8be237b29b73ec843d3bdc9477f0927b
tibor19 <tibi@covaci.se>
2022-06-28 13:12:38 +0200

    Two commits in one

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello dotNET2022!");
+app.MapGet("/", () => "Hello dotNET2022 - now with staging!");
 
 app.Run();
